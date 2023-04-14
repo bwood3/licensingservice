24efea1bccd2cd36a7b072b2bedcba11dd0a9852
Wood, Bryan Michael <woodbrmi@iu.edu>
2023-04-14 14:34:44 -0400

    Initial commit

---
+++ src/main/java/c322/assetManagement/licensingservice/LicensingserviceApplication.java
@@ -0,0 +1,12 @@
+package c322.assetManagement.licensingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensingserviceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LicensingserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/c322/assetManagement/licensingservice/LicensingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package c322.assetManagement.licensingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicensingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
