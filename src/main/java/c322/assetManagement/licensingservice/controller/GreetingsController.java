55fb7fab990fc7bccfdf9c7ab8bb3c62bdea13e0
bwood3 <bryanwood1207@gmail.com>
2023-04-14 21:32:37 -0400

    application.yml fix

---
+++ src/main/java/c322/assetManagement/licensingservice/controller/GreetingsController.java
@@ -0,0 +1,16 @@
+package c322.assetManagement.licensingservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class GreetingsController {
+
+    @GetMapping
+    public String greetings() {
+        return "Greetings from the licensing service!";
+    }
+
+}
